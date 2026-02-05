7b716dc93e6f642b1cd84ceaa657c689f724e7ff
rahimuj570 <rahimuj570@gmail.com>
2026-02-05 17:59:09 +0600

    feat: integrate Firebase core and crashlytics with necessary configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mvvm_task_management/app/app.dart';
+import 'package:mvvm_task_management/firebase_options.dart';
 import 'package:mvvm_task_management/services/notification_servicee.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await NotificationService().initialize();
+
   runApp(const TodoApp());
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,6 +6,8 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import firebase_core
+import firebase_crashlytics
 import flutter_local_notifications
 import flutter_timezone
 import shared_preferences_foundation
@@ -13,6 +15,8 @@ import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseCrashlyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCrashlyticsPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
